6ae3234e73749abec180b59797f9e6d2a5133e38
Giancarlo Soverini <giancarlosoverini@gmail.com>
2016-07-10 18:01:02 +0200

    refactor: Refactor getList method

--- dist/index.mjs
+++ dist/index.mjs
@@ -17,16 +17,36 @@ function extend(a, b) {
     return a;
 }
 
+// Default settings
 var defaults = {
+
+    // The actual speed dial button default class
     button: '.speed-dial__btn',
+
+    // Default option-list class
     list: '.speed-dial__list',
+
+    // Default option-list direction
     direction: 'up'
 };
 
 var SpeedDial = {
+    /**
+    * Method called for initializing a new instance of SpeedDial
+    *
+    * @param { String } selector This is the selector
+    *   of the speed dial button container
+    * @param { Object } options Object to override the default settings
+    */
     init: function init(selector, options) {
 
+        // Override defauls
         this.settings = extend(defaults, options);
+
+        /**
+        * Get the HTMLElement which contains both the button
+        *    and the option-list
+        */
         this.container = document.querySelector(selector);
 
         try {
@@ -38,59 +58,89 @@ var SpeedDial = {
         }
     },
 
+    /**
+    * Get the container
+    *
+    * @return { HTMLElement }
+    */
     getContainer: function getContainer() {
         return this.container;
     },
 
+    /**
+    * Get the button which triggers the option-list to open/close
+    *
+    * @return { HTMLElement }
+    */
     getButton: function getButton() {
-        return this.container.querySelector(this.settings.button);
-    },
-
-    getList: function getList() {
-        return this.container.querySelector(this.settings.list);
-    },
-
-    bindEvents: function bindEvents() {
-        var btn = this.getButton();
-
+        var btn = this.container.querySelector(this.settings.button);
         try {
             if (btn === null) throw new Error('SpeedDial could not find any button.');
-            btn.addEventListener('click', this.clickHandler.bind(this));
         } catch (err) {
             console.log(err.message);
+        } finally {
+            return btn;
         }
     },
 
+    /**
+    * Get the list which contains the speed dial options
+    *
+    * @return { HTMLElement }
+    */
+    getList: function getList() {
+        var list = this.container.querySelector(this.settings.list);
+        try {
+            if (list === null) throw new Error('SpeedDial could not find any option-list.');
+        } catch (err) {
+            console.log(err.message);
+        } finally {
+            return list;
+        }
+    },
+
+    // Add click listener to the speed-dial button
+    bindEvents: function bindEvents() {
+        var btn = this.getButton();
+        btn.addEventListener('click', this.clickHandler.bind(this));
+    },
+
+    // Method called when speed-dial button is clicked
     clickHandler: function clickHandler() {
         var listEl = this.getList();
 
-        if (listEl) {
-            if (listEl.classList.contains('is-visible')) {
-                listEl.classList.remove('is-visible');
-            } else {
-                listEl.classList.add('is-visible');
-            }
-        }
-    },
-
-    close: function close() {
-        var listEl = this.getList();
-
-        if (listEl) {
+        if (listEl.classList.contains('is-visible')) {
+            // Close the option-list
             listEl.classList.remove('is-visible');
-        }
-    },
-
-    open: function open() {
-        var listEl = this.getList();
-
-        if (listEl) {
+        } else {
+            // Open the option-list
             listEl.classList.add('is-visible');
         }
     },
 
+    // Close the option-list
+    close: function close() {
+        var listEl = this.getList();
+        listEl.classList.remove('is-visible');
+    },
+
+    // Open the option-list
+    open: function open() {
+        var listEl = this.getList();
+        listEl.classList.add('is-visible');
+    },
+
+    /**
+    * Speed dial option-list can be viewed in 4 different directions:
+    *   'up', 'down', 'left', 'right'
+    *   The CSS class responsible to handle the option-list direction is added
+    *   to the container
+    *
+    * @param { String } direction
+    */
     setDirection: function setDirection(direction) {
 
+        // Clean the classList first
         this.container.classList.remove('speed-dial--up', 'speed-dial--down', 'speed-dial--left', 'speed-dial--right');
 
         switch (direction) {
@@ -111,13 +161,19 @@ var SpeedDial = {
                 break;
         }
 
+        // Call an helper to set some style with Javascript
         this.styleUpdate();
     },
 
+    // @return { String }
     getDirection: function getDirection() {
         return this.settings.direction;
     },
 
+    /**
+    * Left and right directions require some style computation
+    *   in order to properly show the option-list
+    */
     styleUpdate: function styleUpdate() {
         var listEl = this.getList();
         var direction = this.getDirection();
@@ -140,6 +196,15 @@ var SpeedDial = {
 
 };
 
+/**
+* This is the function invoked for creating a new instance of speed dial button
+*
+* @param { String } selector This is the selector
+*   of the speed dial button container
+* @param { Object } options Object to override the default settings
+* @return { Object } Returns an object which exposes the
+*   SpeedDial public methods
+*/
 function index(selector, options) {
 
     var sd = Object.create(SpeedDial);
@@ -174,6 +239,7 @@ function index(selector, options) {
         return sd.open();
     }
 
+    // Return an object in a pseudo-module pattern way
     return {
         getContainer: getContainer,
         getButton: getButton,
