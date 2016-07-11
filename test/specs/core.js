90342ac5bf63c215193b760ef4ec3144e6114ede
Giancarlo Soverini <giancarlosoverini@gmail.com>
2016-07-11 11:23:11 +0200

    test: Add some tests

--- test/specs/core.js
+++ test/specs/core.js
@@ -1,6 +1,19 @@
 'use strict';
 
-describe('HTML loading', function() {
+describe('Speed Dial', function() {
+
+    var instance;
+
+    /**
+    * http://stackoverflow.com/a/25317941
+    * Check if an object is a DOM-object
+    *
+    * @param { Object } e
+    * @return { Boolean }
+    */
+    function isDOM(e) {
+        return (/HTML(?:.*)Element/).test(Object.prototype.toString.call(e).slice(8, -1));
+    }
 
     before(function() {
         fixture.setBase('test/templates');
@@ -8,16 +21,83 @@ describe('HTML loading', function() {
 
     beforeEach(function() {
         this.result = fixture.load('core.html');
-        console.log(this.result);
+        instance = speeddial('.speed-dial', { direction: 'down' });
     });
 
     afterEach(function() {
         fixture.cleanup();
     });
 
-    describe('#indexOf()', function() {
-        it('plays with the html fixture', function() {
-            expect(fixture.el.firstChild).to.equal(this.result[0]);
-        });
+    it('has to be a function', function () {
+        assert.typeOf(speeddial, 'function');
     });
+
+    it('has to return an object', function () {
+        assert.typeOf(instance, 'object');
+    });
+
+    describe('Public method', function() {
+
+        it('getContainer() returns an HTMLElement', function() {
+            var container = instance.getContainer();
+            expect(isDOM(container)).to.be.true;
+        });
+
+        it('getButton() returns an HTMLElement', function() {
+            var btn = instance.getButton();
+            expect(isDOM(btn)).to.be.true;
+        });
+
+        it('getList() returns an HTMLElement', function() {
+            var list = instance.getList();
+            expect(isDOM(list)).to.be.true;
+        });
+
+        it('open() adds .is-visible class', function() {
+            var list = instance.getList();
+            instance.open();
+            expect(list.classList.contains('is-visible')).to.be.true;
+        });
+
+        it('close() removes .is-visible class', function() {
+            var list = instance.getList();
+            instance.close();
+            expect(list.classList.contains('is-visible')).to.be.false;
+        });
+
+        describe('direction-related', function() {
+            it('getDirection() returns down for this instance', function() {
+                expect(instance.getDirection()).to.equal('down');
+            });
+
+            it('expects SpeedDial container to have .speed-dial--down class', function() {
+                var container = instance.getContainer();
+                expect(container.classList.contains('speed-dial--down')).to.be.true;
+            });
+
+            describe('setDirection(\'left\')', function() {
+
+                beforeEach(function() {
+                    instance.setDirection('left');
+                });
+
+                it('removes .speed-dial--down class', function() {
+                    var container = instance.getContainer();
+                    expect(container.classList.contains('speed-dial--down')).to.be.false;
+                });
+
+                it('adds .speed-dial--left class', function() {
+                    var container = instance.getContainer();
+                    expect(container.classList.contains('speed-dial--left')).to.be.true;
+                });
+
+                it('expects direction in settings to be left', function() {
+                    expect(instance.getDirection()).to.equal('left');
+                });
+            });
+
+        });
+
+    });
+
 });
