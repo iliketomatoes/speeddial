9db59be951bafaed42734fab5292ad1fd7c633ca
Giancarlo Soverini <giancarlosoverini@gmail.com>
2016-07-12 17:49:27 +0200

    refactor: Refactor some CSS rules

--- dist/speeddial.js
+++ dist/speeddial.js
@@ -1,7 +1,7 @@
 /**
   * speeddial - Speed dial button inspired by Google Material design
   *
-  * @version v0.2.0
+  * @version v0.2.1
   * @homepage https://github.com/iliketomatoes/speeddial
   * @license MIT
   * @author Giancarlo Soverini
